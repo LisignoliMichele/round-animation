4c81a7a1aca498c9d07ef033fe42ec9077f9857f
Michele Lisignoli <michele@lisignoli.com>
2021-09-14 17:21:17 +0200

    white lines

--- src/components/round/round.component.jsx
+++ src/components/round/round.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { ReactComponent as RoundSVG } from '../../assets/røund.svg'
+import WhiteLines from "../white-lines/white-lines.component";
+
 
 import './round.styles.scss'
 
 
 const Round = () => (
  <div className="round">
+   <WhiteLines />
    <RoundSVG />
  </div>
 );
---
+++ src/components/white-lines/white-lines.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './white-lines.styles.scss'
+
+const WhiteLines = () => (
+   <div className='white-lines'>
+      <div className='vertical'></div>
+      <div className='horizontal'></div>
+   </div>
+);
+
+export default WhiteLines;
